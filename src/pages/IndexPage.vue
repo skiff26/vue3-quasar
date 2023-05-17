f44d1138f35ea96b7dc612ed730a13fb05d7d1e2
Artem <artem.kulchitsky@gmail.com>
2023-05-17 19:21:13 +0200

    Add page You

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,4 +1,10 @@
 <template>
+	<q-header class="header">
+		<nav class="nav"><a href="#">Главная</a> / <a href="#">Профиль пользователя Иванов Степан </a></nav>
+		<nav class="nav-mobile"><a href="#">Главная</a> / <a href="#">Личный кабинет</a> / <a href="">Профиль
+				пользователя</a>
+		</nav>
+	</q-header>
 	<main class="main">
 		<TheProfileTitle />
 		<TheUser />
--- src/router/routes.js
+++ src/router/routes.js
@@ -5,6 +5,11 @@ const routes = [
     children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   {
+    path: "/you",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/YouPage.vue") }],
+  },
+  {
     path: "/:catchAll(.*)*",
     component: () => import("pages/ErrorNotFound.vue"),
   },
